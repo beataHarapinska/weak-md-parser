c2443a71ba2f6472d7d6c52e7712fa5cf4733896
BBH <beata.harapinska@gmail.com>
2023-03-27 12:18:38 +0200

    Tests refactoring: rename + parametrize

--- src/test/java/com/github/arena/challenges/weakmdparser/MarkdownParserTest.java
+++ src/test/java/com/github/arena/challenges/weakmdparser/MarkdownParserTest.java
@@ -1,121 +1,39 @@
 package com.github.arena.challenges.weakmdparser;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class MarkdownParserTest {
 
-    private MarkdownParser markdownParser;
+    private final MarkdownParser markdownParser = new MarkdownParser();
 
-    @BeforeEach
-    public void setup() {
-        markdownParser = new MarkdownParser();
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments("This will be a paragraph", "<p>This will be a paragraph</p>"),
+                arguments("_This will be italic_", "<p><em>This will be italic</em></p>"),
+                arguments("__This will be bold__", "<p><strong>This will be bold</strong></p>"),
+                arguments("This will _be_ __mixed__", "<p>This will <em>be</em> <strong>mixed</strong></p>"),
+                arguments("# This will be an h1", "<h1>This will be an h1</h1>"),
+                arguments("## This will be an h2", "<h2>This will be an h2</h2>"),
+                arguments("###### This will be an h6",  "<h6>This will be an h6</h6>"),
+                arguments("* Item 1\n* Item 2","<ul><li>Item 1</li><li>Item 2</li></ul>"),
+                arguments("# Header!\n* __Bold Item__\n* _Italic Item_","<h1>Header!</h1><ul><li><strong>Bold Item</strong></li><li><em>Italic Item</em></li></ul>"),
+                arguments("# This is a header with # and * in the text", "<h1>This is a header with # and * in the text</h1>"),
+                arguments("* Item 1 with a # in the text\n* Item 2 with * in the text", "<ul><li>Item 1 with a # in the text</li><li>Item 2 with * in the text</li></ul>"),
+                arguments("This is a paragraph with # and * in the text", "<p>This is a paragraph with # and * in the text</p>"),
+                arguments("# Start a list\n* Item 1\n* Item 2\nEnd a list", "<h1>Start a list</h1><ul><li>Item 1</li><li>Item 2</li></ul><p>End a list</p>")
+        );
     }
 
-    @Test
-    public void normalTextAsAParagraph() {
-        String input = "This will be a paragraph";
-        String expected = "<p>This will be a paragraph</p>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void italics() {
-        String input = "_This will be italic_";
-        String expected = "<p><em>This will be italic</em></p>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void boldText() {
-        String input = "__This will be bold__";
-        String expected = "<p><strong>This will be bold</strong></p>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void normalItalicsAndBoldText() {
-        String input = "This will _be_ __mixed__";
-        String expected = "<p>This will <em>be</em> <strong>mixed</strong></p>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void withH1HeaderLevel() {
-        String input = "# This will be an h1";
-        String expected = "<h1>This will be an h1</h1>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void withH2HeaderLevel() {
-        String input = "## This will be an h2";
-        String expected = "<h2>This will be an h2</h2>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void withH6HeaderLevel() {
-        String input = "###### This will be an h6";
-        String expected = "<h6>This will be an h6</h6>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void unorderedLists() {
-        String input = "* Item 1\n* Item 2";
-        String expected = "<ul><li>Item 1</li><li>Item 2</li></ul>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void aLittleBitOfEverything() {
-        String input = "# Header!\n* __Bold Item__\n* _Italic Item_";
-        String expected = "<h1>Header!</h1><ul><li><strong>Bold Item</strong></li><li><em>Italic Item</em></li></ul>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void markdownSymbolsInTheHeaderShouldNotBeInterpreted() {
-        String input = "# This is a header with # and * in the text";
-        String expected = "<h1>This is a header with # and * in the text</h1>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void markdownSymbolsInTheListItemTextShouldNotBeInterpreted() {
-        String input = "* Item 1 with a # in the text\n* Item 2 with * in the text";
-        String expected = "<ul><li>Item 1 with a # in the text</li><li>Item 2 with * in the text</li></ul>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void markdownSymbolsInTheParagraphTextShouldNotBeInterpreted() {
-        String input = "This is a paragraph with # and * in the text";
-        String expected = "<p>This is a paragraph with # and * in the text</p>";
-
-        assertEquals(expected, markdownParser.parse(input));
-    }
-
-    @Test
-    public void markdownUnorderedListsCloseProperlyWithPrecedingAndFollowingLines() {
-        String input = "# Start a list\n* Item 1\n* Item 2\nEnd a list";
-        String expected = "<h1>Start a list</h1><ul><li>Item 1</li><li>Item 2</li></ul><p>End a list</p>";
-
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void shouldTranslateStringWithMarkdownSyntaxIntoHtmlString(String input, String expected) {
         assertEquals(expected, markdownParser.parse(input));
     }
 }
--- src/test/java/com/github/arena/challenges/weakmdparser/WeakMdParserApplicationIntegrationTest.java
+++ src/test/java/com/github/arena/challenges/weakmdparser/WeakMdParserApplicationIntegrationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WeakMdParserApplicationTests {
+class WeakMdParserApplicationIntegrationTest {
 
     @Test
     void contextLoads() {
